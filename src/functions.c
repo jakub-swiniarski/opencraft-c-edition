2b4464a505b84b82ad198526df037b8422be2197
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-01-22 20:38:33 +0100

    simplify mouse function

--- src/functions.c
+++ src/functions.c
@@ -82,151 +82,85 @@ void draw_block(Texture *texture, int x, int y, int z, int *sides)
 
 
 
-Vector3 GetCameraForward(Camera *camera)
+Vector3 GetCameraForward(Camera *cam)
 {
-    return Vector3Normalize(Vector3Subtract(camera->target, camera->position));
+    return Vector3Normalize(Vector3Subtract(cam->target, cam->position));
 }
 
 
-Vector3 GetCameraUp(Camera *camera)
+Vector3 GetCameraUp(Camera *cam)
 {
-    return Vector3Normalize(camera->up);
+    return Vector3Normalize(cam->up);
 }
 
-Vector3 GetCameraRight(Camera *camera)
+Vector3 GetCameraRight(Camera *cam)
 {
-    Vector3 forward = GetCameraForward(camera);
-    Vector3 up = GetCameraUp(camera);
+    Vector3 forward = GetCameraForward(cam);
+    Vector3 up = GetCameraUp(cam);
 
     return Vector3CrossProduct(forward, up);
 }
 
-void CameraYaw(Camera *camera, float angle, bool rotateAroundTarget)
+void CameraYaw(Camera *cam, float angle)
 {
-    // Rotation axis
-    Vector3 up = GetCameraUp(camera);
-
-    // View vector
-    Vector3 targetPosition = Vector3Subtract(camera->target, camera->position);
-
-    // Rotate view vector around up axis
+    Vector3 up = GetCameraUp(cam);
+    Vector3 targetPosition = Vector3Subtract(cam->target, cam->position);
     targetPosition = Vector3RotateByAxisAngle(targetPosition, up, angle);
-
-    if (rotateAroundTarget)
-    {
-        // Move position relative to target
-        camera->position = Vector3Subtract(camera->target, targetPosition);
-    }
-    else // rotate around camera.position
-    {
-        // Move target relative to position
-        camera->target = Vector3Add(camera->position, targetPosition);
-    }
+    cam->target = Vector3Add(cam->position, targetPosition);
 }
 
-void CameraPitch(Camera *camera, float angle, bool lockView, bool rotateAroundTarget, bool rotateUp)
+void CameraPitch(Camera *cam, float angle, bool rotateUp)
 {
-    // Up direction
-    Vector3 up = GetCameraUp(camera);
-
-    // View vector
-    Vector3 targetPosition = Vector3Subtract(camera->target, camera->position);
-
-    if (lockView)
-    {
-        // In these camera modes we clamp the Pitch angle
-        // to allow only viewing straight up or down.
-
-        // Clamp view up
-        float maxAngleUp = Vector3Angle(up, targetPosition);
-        maxAngleUp -= 0.001f; // avoid numerical errors
-        if (angle > maxAngleUp) angle = maxAngleUp;
-
-        // Clamp view down
-        float maxAngleDown = Vector3Angle(Vector3Negate(up), targetPosition);
-        maxAngleDown *= -1.0f; // downwards angle is negative
-        maxAngleDown += 0.001f; // avoid numerical errors
-        if (angle < maxAngleDown) angle = maxAngleDown;
-    }
-
-    // Rotation axis
-    Vector3 right = GetCameraRight(camera);
-
-    // Rotate view vector around right axis
+    Vector3 up = GetCameraUp(cam);
+    Vector3 targetPosition = Vector3Subtract(cam->target, cam->position);
+    float maxAngleUp = Vector3Angle(up, targetPosition);
+    maxAngleUp -= 0.001f;
+    if (angle > maxAngleUp) angle = maxAngleUp;
+    float maxAngleDown = Vector3Angle(Vector3Negate(up), targetPosition);
+    maxAngleDown *= -1.0f;
+    maxAngleDown += 0.001f;
+    if (angle < maxAngleDown) angle = maxAngleDown;
+    Vector3 right = GetCameraRight(cam);
     targetPosition = Vector3RotateByAxisAngle(targetPosition, right, angle);
-
-    if (rotateAroundTarget)
-    {
-        // Move position relative to target
-        camera->position = Vector3Subtract(camera->target, targetPosition);
-    }
-    else // rotate around camera.position
-    {
-        // Move target relative to position
-        camera->target = Vector3Add(camera->position, targetPosition);
-    }
-
+    cam->target = Vector3Add(cam->position, targetPosition);
     if (rotateUp)
-    {
-        // Rotate up direction around right axis
-        camera->up = Vector3RotateByAxisAngle(camera->up, right, angle);
-    }
+        cam->up = Vector3RotateByAxisAngle(cam->up, right, angle);
 }
 
-void CameraMoveForward(Camera *camera, float distance, bool moveInWorldPlane)
+void CameraMoveForward(Camera *cam, float distance, bool moveInWorldPlane)
 {
-    Vector3 forward = GetCameraForward(camera);
-
+    Vector3 forward = GetCameraForward(cam);
     if (moveInWorldPlane)
     {
-        // Project vector onto world plane
         forward.y = 0;
         forward = Vector3Normalize(forward);
     }
-
-    // Scale by distance
     forward = Vector3Scale(forward, distance);
-
-    // Move position and target
-    camera->position = Vector3Add(camera->position, forward);
-    camera->target = Vector3Add(camera->target, forward);
+    cam->position = Vector3Add(cam->position, forward);
+    cam->target = Vector3Add(cam->target, forward);
 }
 
-void CameraMoveRight(Camera *camera, float distance, bool moveInWorldPlane)
+void CameraMoveRight(Camera *cam, float distance, bool moveInWorldPlane)
 {
-    Vector3 right = GetCameraRight(camera);
-
+    Vector3 right = GetCameraRight(cam);
     if (moveInWorldPlane)
     {
-        // Project vector onto world plane
         right.y = 0;
         right = Vector3Normalize(right);
     }
-
-    // Scale by distance
     right = Vector3Scale(right, distance);
-
-    // Move position and target
-    camera->position = Vector3Add(camera->position, right);
-    camera->target = Vector3Add(camera->target, right);
+    cam->position = Vector3Add(cam->position, right);
+    cam->target = Vector3Add(cam->target, right);
 }
 
-
-void update_camera(Camera *camera){
+void update_camera(Camera *cam){
     Vector2 mousePositionDelta = GetMouseDelta();
 
-    bool moveInWorldPlane = 1;
-    bool rotateAroundTarget = 0;
-    bool lockView = 1;
-    bool rotateUp = false;
+    CameraYaw(cam, -mousePositionDelta.x*MOUSE_SENS);
+    CameraPitch(cam, -mousePositionDelta.y*MOUSE_SENS, 0);
 
-    // Mouse support
-    CameraYaw(camera, -mousePositionDelta.x*MOUSE_SENS, rotateAroundTarget);
-    CameraPitch(camera, -mousePositionDelta.y*MOUSE_SENS, lockView, rotateAroundTarget, rotateUp);
-
-    // Keyboard support
-    if (IsKeyDown(KEY_W)) CameraMoveForward(camera, MOVE_SPEED, moveInWorldPlane);
-    if (IsKeyDown(KEY_A)) CameraMoveRight(camera, -MOVE_SPEED, moveInWorldPlane);
-    if (IsKeyDown(KEY_S)) CameraMoveForward(camera, -MOVE_SPEED, moveInWorldPlane);
-    if (IsKeyDown(KEY_D)) CameraMoveRight(camera, MOVE_SPEED, moveInWorldPlane);
+    if (IsKeyDown(KEY_W)) CameraMoveForward(cam, MOVE_SPEED, 1);
+    if (IsKeyDown(KEY_A)) CameraMoveRight(cam, -MOVE_SPEED, 1);
+    if (IsKeyDown(KEY_S)) CameraMoveForward(cam, -MOVE_SPEED, 1);
+    if (IsKeyDown(KEY_D)) CameraMoveRight(cam, MOVE_SPEED, 1);
 }
